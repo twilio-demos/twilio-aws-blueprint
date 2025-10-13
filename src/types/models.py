ae37e15ebc1ce93e608aaa7f86c9d63d1b6ce22a
Sam Johnson <sam@scj.me>
2025-10-13 13:59:16 -0500

    Support pipe-delimited multi-language config

--- src/types/models.py
+++ src/types/models.py
@@ -8,7 +8,8 @@ from src.utils.env import (
     IDLE_MAX_ATTEMPTS,
     IDLE_TIMEOUT,
     INITIAL_HINTS,
-    TTS_LANGUAGE,
+    LANGUAGE,
+    SPLIT_CHAR,
 )
 
 
@@ -36,8 +37,8 @@ class SessionIdleConfig(BaseModel):
 class SessionConfig(BaseModel):
     DTMF: SessionDTMFConfig = SessionDTMFConfig()
     Idle: SessionIdleConfig = SessionIdleConfig()
-    Hints: str = INITIAL_HINTS
-    Lang: str = TTS_LANGUAGE
+    Hints: str = INITIAL_HINTS.split(SPLIT_CHAR)[0]
+    Lang: str = LANGUAGE.split(SPLIT_CHAR)[0]
 
 
 class Session(BaseModel):
@@ -47,7 +48,7 @@ class Session(BaseModel):
     SessionStatus: str = "in-progress"
     Created: str
     Config: SessionConfig = SessionConfig()
-    SessionState: dict[str, str | int | bool] | None = None
+    SessionState: dict[str, str | int | bool] = {}
 
 
 class MessageType(str, Enum):
--- src/utils/twiml.py
+++ src/utils/twiml.py
@@ -4,7 +4,10 @@ from twilio.twiml.voice_response import VoiceResponse
 
 from src.utils.env import (
     INITIAL_HINTS,
-    TTS_LANGUAGE,
+    LANGUAGE,
+    SPEECH_MODEL,
+    SPLIT_CHAR,
+    TRANSCRIPTION_PROVIDER,
     TTS_PROVIDER,
     TTS_VOICE,
 )
@@ -31,21 +34,74 @@ def create_initial_twiml(
     connect_action_url = action_url or f"https://{host}/call/action"
     connect = response.connect(action=connect_action_url)
 
+    languages = (language or LANGUAGE).split(SPLIT_CHAR)
+    env_languages = LANGUAGE.split(SPLIT_CHAR)
+    hints_list = (hints or INITIAL_HINTS).split(SPLIT_CHAR)
+    tts_providers = TTS_PROVIDER.split(SPLIT_CHAR)
+    transcription_providers = TRANSCRIPTION_PROVIDER.split(SPLIT_CHAR)
+    speech_models = SPEECH_MODEL.split(SPLIT_CHAR)
+    voices = TTS_VOICE.split(SPLIT_CHAR)
+
+    custom_language = False
+    custom_language_index = 0
+
+    if len(languages) == 1 and len(env_languages) > 1 and languages[0] in env_languages:
+        # If a specific language was passed to this function, we want to use that language's configuration for TTS and STT parameters.
+        custom_language = True
+        custom_language_index = env_languages.index(languages[0])
+
+    tts_provider = tts_providers[0]
+    voice = voices[0]
+    transcription_provider = transcription_providers[0]
+    speech_model = speech_models[0]
+    custom_hints = hints_list[0]
+
+    if custom_language:
+        # Use setting per language if defined
+        if len(tts_providers) > custom_language_index:
+            tts_provider = tts_providers[custom_language_index]
+        if len(voices) > custom_language_index:
+            voice = voices[custom_language_index]
+        if len(transcription_providers) > custom_language_index:
+            transcription_provider = transcription_providers[custom_language_index]
+        if len(speech_models) > custom_language_index:
+            speech_model = speech_models[custom_language_index]
+        if len(hints_list) > custom_language_index:
+            custom_hints = hints_list[custom_language_index]
+
     conversation_relay = connect.conversation_relay(
         url=websocket_url,
         dtmf_detection=True,
         interruptible="any",
         welcome_greeting=welcome_greeting or "",
-        language=language or TTS_LANGUAGE,
-        tts_provider=TTS_PROVIDER,
-        voice=TTS_VOICE,
-        hints=hints or INITIAL_HINTS,
+        transcription_language=languages[0],
+        tts_language=languages[0],
+        tts_provider=tts_provider,
+        voice=voice,
+        transcription_provider=transcription_provider,
+        speech_model=speech_model,
+        hints=custom_hints,
     )
 
+    if len(env_languages) > 1:
+        for index in range(len(env_languages)):
+            if env_languages[index] == "multi":
+                continue
+            conversation_relay.language(
+                env_languages[index],
+                tts_providers[index],
+                voices[index],
+                transcription_providers[index],
+                speech_models[index],
+            )
+
     # Store initial settings as parameters so that we can receive them in the setup message
-    conversation_relay.parameter(name="initial_hints", value=hints)
-    conversation_relay.parameter(name="initial_language", value=language)
-    conversation_relay.parameter(name="initial_greeting", value=welcome_greeting)
+    conversation_relay.parameter(
+        name="initial_hints",
+        value=custom_hints,
+    )
+    conversation_relay.parameter(name="initial_language", value=languages[0])
+    conversation_relay.parameter(name="initial_greeting", value=welcome_greeting or "")
 
     if params is not None:
         for param, value in params.items():
